24a96ad05e1bddddc7d3a329430cc398ad79e96c
Kevin <krocca@zagmail.gonzaga.edu>
2023-02-04 16:25:12 -0800

    Update code befor major changes

--- AllDice.java
+++ AllDice.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
+/*
+ * This class takes all 5 dice at the same time to make things easier.
+*/
+
 public class AllDice {
     private Die die1; 
     private Die die2; 
@@ -5,7 +11,8 @@ public class AllDice {
     private Die die4; 
     private Die die5; 
     private int diceSidesUp[];
-    
+    private ArrayList<Integer> diceToKeep;
+
     public AllDice()
     {
         this.die1 = new Die();
@@ -14,6 +21,7 @@ public class AllDice {
         this.die4 = new Die();
         this.die5 = new Die();
         diceSidesUp = new int[5];
+        diceToKeep = new ArrayList<Integer>();
     }
 
     public void rollAll()
@@ -25,14 +33,19 @@ public class AllDice {
         die5.roll();
     }
 
+    //unsure if needed. Might use later.
     public int[] getAllSides()
+    {
+        return diceSidesUp;
+    }
+
+    public void updateSidesUp()
     {
         diceSidesUp[0] = die1.getSideUp();
         diceSidesUp[1] = die2.getSideUp();
         diceSidesUp[2] = die3.getSideUp();
         diceSidesUp[3] = die4.getSideUp();
         diceSidesUp[4] = die5.getSideUp();
-        return diceSidesUp;
     }
 
     public void printAll()
@@ -41,12 +54,85 @@ public class AllDice {
         {
             System.out.print(diceSidesUp[i] + "\t");
         }
+        System.out.println();
     }
 
-    //make this a generic function if possible
-    public void roll1()
+
+
+
+    //Roll each die individually
+    private void roll1()
     {
         die1.roll();
     }
 
+    private void roll2()
+    {
+        die2.roll();
+    }
+
+    private void roll3()
+    {
+        die3.roll();
+    }
+
+    private void roll4()
+    {
+        die4.roll();
+    }
+
+    private void roll5()
+    {
+        die5.roll();
+    }
+
+
+
+
+    //ArrayList section about diceToKeep
+    public void addToKeep(int keep)
+    {
+        this.diceToKeep.add(keep);
+    }
+    
+    //This function checks to see if diceToKeep DOESNT have a number. It then
+    //Calls the rollRemainingSwitch to roll that specific die.
+    public void rollRemaining()
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            if(!diceToKeep.contains(i))
+            {
+                rollRemainingSwitch(i);
+            }
+        }
+    }
+
+    private void rollRemainingSwitch(int i)
+    {
+        switch(i)
+        {
+            case 1:
+                roll1();
+                break;
+            case 2:
+                roll2();
+                break;
+            case 3:
+                roll3();
+                break;
+            case 4:
+                roll4();
+                break;
+            case 5:
+                roll5();
+                break;
+        }
+    }
+
+    //reset the list after next roll.
+    public void clearList()
+    {
+        this.diceToKeep.clear();
+    }
 }
--- Yahtzee.java
+++ Yahtzee.java
@@ -1,22 +1,71 @@
 import java.util.Scanner;
 public class Yahtzee {
+    //Yahtzee has a alldice. I think it needs static though I'm not sure.
+    private static AllDice allDice;
+
     public static void main(String[] args) {
-        AllDice allDice = new AllDice();
+        allDice = new AllDice();
         Scanner sc = new Scanner(System.in);
-        coreGameLoop(sc, allDice);
+        while(sc.next().equalsIgnoreCase("y"))
+        {
+            coreGameLoop(sc); //run this continuously for the thirteen rounds for actual game when i make it.
+            System.out.println("Play Again? y/n");
+        }
+        sc.close();
+    }
+
+
+
+    public static void coreGameLoop(Scanner sc)
+    {
+        allDice.rollAll(); //get the game started
+
+        //This should print the dice sides up, roll whatever dice the user didn't
+        //want to keep, then update the sides up until the turn ends.
+        for(int i = 0; i < 2; i++)
+        {
+            allDice.updateSidesUp();
+            System.out.println("Choose which dice to keep (ex. ynyny to keep 1 3 and 5)");
+            allDice.printAll();
+            updateDiceToKeep(sc);
+            allDice.rollRemaining();
+            allDice.clearList();
+            System.out.println();
+        }
+        
+        //This is here because if i let the for loop go again it would roll
+        //an extra time and ask for which dice to keep.
+        System.out.println("Final roll results:");
+        allDice.updateSidesUp();
+        allDice.printAll();
+        System.out.println();
+
+        //ask the user which category to put their score into
+    }
+
+    
+
+    public static void calculateScores()
+    {
 
     }
 
-    public static void coreGameLoop(Scanner sc, AllDice allDice)
-    {
-        String diceToKeep;
-        for(int i = 0; i < 3; i++)
-        {
-            allDice.rollAll();
-            System.out.println("Choose which dice to keep (ex. for dice 1, 3, 4 type 1 3 4)");
-            allDice.printAll();
-            diceToKeep = sc.nextLine();
 
+
+    //This function takes the user input of which dice to keep and splits
+    //it into an array using a space as a delimiter. It then sends
+    //the value to the alldice object to update the diceToKeep arrayList.
+    public static void updateDiceToKeep(Scanner sc)
+    {
+        String diceToKeep = sc.nextLine();
+        int keep;
+        String[] diceNumber = diceToKeep.split(" ");
+
+
+        for(int i = 0; i < diceNumber.length; i++)
+        {
+            keep = Integer.parseInt(diceNumber[i]);
+            allDice.addToKeep(keep);      
         }
     }
 }
